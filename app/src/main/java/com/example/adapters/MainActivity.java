38ca64e2107edbcfcae814a70995e584720bdc4f
Hiba Saqib <hibasaqib72@gmail.com>
2021-11-17 21:42:05 +0500

    Add a friend functionality added

--- app/src/main/java/com/example/adapters/MainActivity.java
+++ app/src/main/java/com/example/adapters/MainActivity.java
@@ -3,18 +3,24 @@ package com.example.adapters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.button);
+        EditText editText=findViewById(R.id.editTextPersonName);
         ListView listView=findViewById(R.id.myListView);
         ArrayList<String> friendList=new ArrayList<String>();
         friendList.add("Irij");
@@ -23,5 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,friendList);
         listView.setAdapter(arrayAdapter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                friendList.add(editText.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
